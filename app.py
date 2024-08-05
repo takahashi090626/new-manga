bb58d5679518018d13e36309e430759ad298e25c
Takahashi_h <148034756+22jy0223@users.noreply.github.com>
2024-08-05 16:00:23 +0900

    部分一致検索実装

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from flask import Flask, render_template, request
+from flask import Flask, request, render_template
 import mysql.connector
 
 app = Flask(__name__)
 
-# データベース接続情報
+# MySQLの接続情報
 db_config = {
-    'host': 'localhost',
     'user': 'root',
     'password': 'root',
+    'host': 'localhost',
     'database': 'comics_db',
     'port':'3307'
 }
@@ -18,17 +18,12 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    query = request.form.get('query', '')
-    if not query:
-        return render_template('index.html', results=[])
-
+    query = request.form['query']
     connection = mysql.connector.connect(**db_config)
     cursor = connection.cursor(dictionary=True)
     cursor.execute("SELECT * FROM comics WHERE title LIKE %s", ('%' + query + '%',))
     results = cursor.fetchall()
-    cursor.close()
     connection.close()
-
     return render_template('results.html', results=results)
 
 if __name__ == '__main__':
