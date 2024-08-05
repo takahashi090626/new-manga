a45f83398d17391c7606546349e63ffd374d1ce1
Takahashi_h <148034756+22jy0223@users.noreply.github.com>
2024-08-05 16:55:11 +0900

    new

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ db_config = {
     'password': 'root',
     'host': 'localhost',
     'database': 'comics_db',
-    'port':'3307'
+    'port': '3307'
 }
 
 @app.route('/')
@@ -18,13 +18,27 @@ def index():
 
 @app.route('/search', methods=['POST'])
 def search():
-    query = request.form['query']
-    connection = mysql.connector.connect(**db_config)
-    cursor = connection.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM comics WHERE title LIKE %s", ('%' + query + '%',))
-    results = cursor.fetchall()
-    connection.close()
+    query = request.form.get('query', '')
+    if not query:
+        return render_template('results.html', results=[])
+
+    try:
+        # データベース接続
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor(dictionary=True)
+        
+        # クエリ実行
+        cursor.execute("SELECT * FROM comics WHERE title LIKE %s", ('%' + query + '%',))
+        results = cursor.fetchall()
+        
+        # データベース接続終了
+        connection.close()
+
+    except mysql.connector.Error as err:
+        print(f"Error: {err}")
+        results = []
+
     return render_template('results.html', results=results)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
